bd5d30f981969d596e54c4cbdbaca7136cb7a880
MoiraQ <mq221@exeter.ac.uk>
2019-10-26 16:43:54 +1000

    did some shit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var happy = 0;
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +18,44 @@ app.post("*", (req, res) => {
 
 app.get("*", (req, res) => {
   // res.status(200).send({ data: "You are meant to send a post request Moira" });
-  res.status(200).send({ data: Date.now() });
+  res.status(200).send({ happy });
 });
+
+function AnonLog() {
+    
+  // Configure the credentials provider to use your identity pool
+  AWS.config.region = 'us-east'; // Region
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:ff8c987b-fd18-4e05-90f2-546ec9548431',
+  });
+  // Make the call to obtain credentials
+  AWS.config.credentials.get(function () {
+    // Credentials will be available when this function is called.
+    var accessKeyId = AWS.config.credentials.accessKeyId;
+    var secretAccessKey = AWS.config.credentials.secretAccessKey;
+    var sessionToken = AWS.config.credentials.sessionToken;
+  });
+}
+
+function ReadFaces(imageData) {
+  AWS.region = "us-east";
+  var rekognition = new AWS.Rekognition();
+  var params = {
+    Image: {
+      Bytes: imageData
+    },
+    Attributes: [
+      'ALL',
+    ]
+  };
+  rekognition.detectFaces(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+      for (var i = 0; i < data.FaceDetails.length; i++) {
+        happy = data.FaceDetails[i].Emotion.Type("HAPPY")
+      }
+    }
+  });
+}
 
 module.exports = app;
